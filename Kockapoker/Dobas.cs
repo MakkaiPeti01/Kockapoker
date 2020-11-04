4250cd5b9e5ec88b5f9a7d992e3878a5883c1abc
MakkaiPeti01 <PetyaPower@gmail.com>
2020-11-04 14:15:40 +0100

    Add project files.

---
+++ Kockapoker/Dobas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kockapoker
+{
+    class Dobas
+    {
+        int[] kockak = new int[5];
+
+        public Dobas()
+        {
+
+        }
+
+        public void EgyDobas()
+        {
+            Random vel = new Random();
+            for (int i = 0; i < kockak.Length; i++)
+            {
+                kockak[i] = vel.Next(1, 7);
+            }
+        }
+
+        public void Kiiras()
+        {
+            foreach (var i in kockak)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        public string Erteke()
+        {
+
+        }
+    }
+}
