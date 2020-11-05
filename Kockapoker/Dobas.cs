35d29d5044ac068014ae2c69625f52dab9a71d6d
MakkaiPeti01 <PetyaPower@gmail.com>
2020-11-05 13:18:17 +0100

    orai

--- Kockapoker/Dobas.cs
+++ Kockapoker/Dobas.cs
@@ -10,7 +10,13 @@ namespace Kockapoker
     class Dobas
     {
         int[] kockak = new int[5];
-
+        private string eredmeny;
+        public string Eredmeny { 
+            get 
+            {
+                return eredmeny;
+            }
+        }
         public Dobas()
         {
 
@@ -22,6 +28,7 @@ namespace Kockapoker
             kockak[2] = k3;
             kockak[3] = k4;
             kockak[4] = k5;
+            eredmeny = Erteke();
         }
         public void EgyDobas()
         {
@@ -30,19 +37,20 @@ namespace Kockapoker
             {
                 kockak[i] = vel.Next(1, 7);
             }
+            eredmeny = Erteke();
         }
 
         public void Kiiras()
         {
             foreach (var i in kockak)
             {
-                Console.Write($"{i} ");
+                Console.Write($"{i}");
             }
+            Console.WriteLine($"-> {eredmeny}");
         }
 
         public string Erteke()
-        {
-            string r = "";
+        {  
             Dictionary<int, int> eredmeny = new Dictionary<int, int>();
             for (int i = 1; i <= 6; i++)
             {
@@ -52,23 +60,81 @@ namespace Kockapoker
             {
                 eredmeny[i]++;
             }
-            if (eredmeny.ContainsValue(5))
+            /*A dicból lekérjük az 1 Valuenal nagyobb elemeket!
+             * Első eset ha egy elem marad: 5-nagypoker, 4-poker, 3-drill, 2-pár
+             * Hanyas (Key) érték határozza meg pl 4 es póker
+             * Második eset 2 elem marad
+             * Value 3 és 2 --> Full
+             * Value 2 és 2 --> Két pár
+             * Harmadik eset nem marad egy sem
+             * Ha key 6 == 0 kis sor vagy key 1 == 0 nagy sor
+             * Minden más --> szemét
+             */
+            var result = (from e in eredmeny
+                         where e.Value > 1
+                         orderby e.Value descending
+                          select new { Szam = e.Key, Db = e.Value }).ToList();
+            Console.WriteLine();
+            int db = result.Count;
+            if (db == 1)
             {
-                r = "Nagypóker";
+                switch (result[0].Db)
+                {
+                    case 5:
+                        return $" {result[0].Szam} Nagypóker";
+                    case 4:
+                        return $"{result[0].Szam} Póker";
+                    case 3:
+                        return $"{result[0].Szam} Drill";
+                    case 2:
+                        return $"{result[0].Szam} Pár";
+                    default:
+                        break;
+                }
             }
-            else if (eredmeny.ContainsValue(4))
+            //vagy 
+            if (db == 1)
             {
-                r = "Póker";
+                string[] egyes = new string[] { "", "", "Pár", "Drill", "Póker", "Nagypóker" };
+                return $"{result[0].Szam} {egyes[result[0].Db]}";
             }
-            else if (eredmeny.ContainsValue(3))
+            else if (db == 2)
             {
-                r = "Drill";
+                if (result[0].Db == 3 && result[1].Db == 2)
+                {
+                    if (result[0].Szam > result[1].Szam)
+                    {
+                        return $"{result[0].Szam}-{result[1].Szam} Full";
+                    }
+                    else
+                    {
+                        return $"{result[1].Szam}-{result[0].Szam} Full";
+                    }
+                }
+                else
+                {
+                    if (result[0].Szam > result[1].Szam)
+                    {
+                        return $"{result[0].Szam}-{result[1].Szam} Pár";
+                    }
+                    else
+                    {
+                        return $"{result[1].Szam}-{result[0].Szam} Pár";
+                    }
+                }
             }
-            else if (eredmeny.ContainsValue(4))
+            else
             {
-                r = "Pár";
+                if (eredmeny[6] == 0)
+                {
+                    return "Kis sor";
+                }
+                else if (eredmeny[1] == 0)
+                {
+                    return "Nagy sor";
+                }
             }
-            return r;
+            return "Szemét";
         }
     }
 }
--- Kockapoker/Program.cs
+++ Kockapoker/Program.cs
@@ -9,13 +9,29 @@ namespace Kockapoker
 {
     class Program
     {
+        static void Ellenorzes()
+        {
+            StreamReader olvas = new StreamReader("Teszteles.txt");
+            while (!olvas.EndOfStream)
+            {
+                string[] a = olvas.ReadLine().Split(',');
+            List<int> sz = a.Select(x => int.Parse(x)).ToList();
+            Dobas d = new Dobas(sz[0], sz[1], sz[2], sz[3], sz[4]);
+            d.Kiiras();
+            }
+        }
         static void Main(string[] args)
         {
-            Dobas d = new Dobas(1,1,1,2,3);
-
-           // d.EgyDobas();
-            d.Kiiras();
-            Console.WriteLine(d.Erteke());
+            //d.EgyDobas();
+            //d.Kiiras();
+            //Console.WriteLine(d.Erteke());
+            //Ellenorzes();
+            Dobas gep = new Dobas();
+            Dobas ember = new Dobas();
+            gep.EgyDobas();
+            ember.EgyDobas();
+            gep.Kiiras();
+            ember.Kiiras();
             Console.ReadKey();
         }
     }
